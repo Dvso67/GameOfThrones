4502645b67101bd11c16b2fb1c155b8f820488b4
yourusername <ararat.markosyan.y@tumo.org>
2022-03-13 17:55:23 +0400

    Lesson 2

--- Programming 2 Final Work/Eater_Class.js
+++ Programming 2 Final Work/Eater_Class.js
@@ -1,50 +1,13 @@
-class Eater{
+class Eater extends LivingCreature {
+
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        //this.index = index;
-        this.energy = 8;
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
+        super(x, y);
+        this.energy = 8; 
 
-    }
-    GetNewCoordinates() {
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-    }
-
-    chooseCharacter(char1){
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == char1) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-        return found;
     }
     
     mul(){
-        var newChar = random(this.chooseCharacter(0));
+        var newChar = random(this.chooseCell(0));
         if (newChar) {
             const newEater = new Eater(newChar[0], newChar[1]);
             eaterArr.push(newEater);
@@ -55,7 +18,7 @@ class Eater{
     
     eat(){
         this.GetNewCoordinates();
-        var newChar = random(this.chooseCharacter(1));
+        var newChar = random(this.chooseCell(1));
         if (newChar){
             this.energy+=2;
             matrix[this.y][this.x] = 0;
@@ -80,7 +43,7 @@ class Eater{
     }
     
     move(){
-        var newChar = random(this.chooseCharacter(0));
+        var newChar = random(this.chooseCell(0));
         this.energy -=2;
         if (this.energy > 0 && newChar){
             matrix[this.y][this.x] = 0;
@@ -89,7 +52,7 @@ class Eater{
             matrix[y][x] = 2;
             this.x = x;
             this.y = y;
-            var newChar2 = random(this.chooseCharacter(1));
+            var newChar2 = random(this.chooseCell(1));
             if (newChar2){
                 this.energy ++;
             }
@@ -98,7 +61,7 @@ class Eater{
             this.die();
         }
         else{
-            var newChar3 = random(this.chooseCharacter(1));
+            var newChar3 = random(this.chooseCell(1));
             if (newChar3){
                 this.energy ++;
             }
--- Programming 2 Final Work/Man_Class.js
+++ Programming 2 Final Work/Man_Class.js
@@ -1,47 +1,13 @@
 class Man {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        //this.index = index;
-        this.energy = 6;
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
+        super(x, y);
+        this.energy = 8; 
+
     }
 
-    GetNewCoordinates() {
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-    }
-
-    chooseCharacter(char1) {
+    chooseCell(ch) {
         this.GetNewCoordinates();
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == char1) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-        return found;
+        return super.chooseCell(ch);
     }
 
     mul() {
